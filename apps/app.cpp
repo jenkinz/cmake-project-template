3a0fd4a72c4b1a1177904a8dc5297446c6c7a56a
Brian Jenkins <bj@jenkinz.com>
2021-02-05 17:31:03 -0800

    Initial commit

---
+++ apps/app.cpp
@@ -0,0 +1,11 @@
+#include "lib/lib.hpp"
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Simple example C++ compiled correctly and ran." << std::endl;
+    std::cout << simple_lib_function() << std::endl;
+
+    return 0;
+}
