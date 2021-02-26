ee96008c5aea4be22cf14190e01c4909496a9ddf
Brian Jenkins <bjenkins@sandel.com>
2021-02-26 14:40:12 -0800

    Update clang-format standards

--- tests/test_mylib.cpp
+++ tests/test_mylib.cpp
@@ -5,30 +5,20 @@
 /// ----------------------------------------------------------------------------
 
 #include "test_mylib.hpp"
-
 #include <gtest/gtest.h>
-
 #include <string>
-
 #include "mylib/mylib.hpp"
 
-std::string const LibTest::kExpectedStringPrefix_ =
-    "Compiled in library, param = ";
+std::string const LibTest::kExpectedStringPrefix_ = "Compiled in library, param = ";
 
-TEST_F(LibTest, DefaultInputEqual) {
-    EXPECT_EQ(mylib::SimpleLibFunction(), kExpectedStringPrefix_ + "Default");
-}
+TEST_F(LibTest, DefaultInputEqual) { EXPECT_EQ(mylib::SimpleLibFunction(), kExpectedStringPrefix_ + "Default"); }
 
-TEST_F(LibTest, DefaultInputNotEqual) {
-    EXPECT_NE(mylib::SimpleLibFunction(), kExpectedStringPrefix_ + "D");
-}
+TEST_F(LibTest, DefaultInputNotEqual) { EXPECT_NE(mylib::SimpleLibFunction(), kExpectedStringPrefix_ + "D"); }
 
 TEST_F(LibTest, CustomInputEqual) {
-    EXPECT_EQ(mylib::SimpleLibFunction("Test"),
-              kExpectedStringPrefix_ + "Test");
+    EXPECT_EQ(mylib::SimpleLibFunction("Test"), kExpectedStringPrefix_ + "Test");
     EXPECT_EQ(mylib::SimpleLibFunction("3"), kExpectedStringPrefix_ + "3");
-    EXPECT_EQ(mylib::SimpleLibFunction("…∑ª©æç≤≈"),
-              kExpectedStringPrefix_ + "…∑ª©æç≤≈");
+    EXPECT_EQ(mylib::SimpleLibFunction("…∑ª©æç≤≈"), kExpectedStringPrefix_ + "…∑ª©æç≤≈");
     EXPECT_EQ(mylib::SimpleLibFunction(""), kExpectedStringPrefix_);
 }
 
