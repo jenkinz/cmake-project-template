37f40d80227fd7a1e3f8e96b45b583e78bfbbce0
Brian Jenkins <bj@jenkinz.com>
2021-07-31 12:23:33 -0700

    Fix coverage task to use make

--- .vscode/run_coverage.sh
+++ .vscode/run_coverage.sh
@@ -4,5 +4,5 @@
 
 mkdir -p build_coverage
 cd build_coverage
-cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --target ctest_coverage
